44723b07d5f8b5210d241125e71678eb438bc4dd
Shahadat227 <shahadathossen227@gmail.com>
2026-03-13 12:46:13 +0600

    changed file path

---
+++ 01_Python_Basics/06_variables_1.py
@@ -0,0 +1,13 @@
+name = "Shahadat"
+age = 22
+country = "Bangladesh"
+print(name)
+print(age)
+print(country)
+
+"""
+output: 
+Shahadat
+22
+Bangladesh
+"""
---
+++ 01_Python_Basics/07_variables_2.py
@@ -0,0 +1,11 @@
+name = "Shahadat"
+age = 22
+
+print("My name is",name)
+print("I am",age,"years old")
+
+"""
+output:
+My name is Shahadat
+I am 22 years old
+"""
